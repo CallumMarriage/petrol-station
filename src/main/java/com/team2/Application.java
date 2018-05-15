6d0373fb977373c6881e86f0825dd09da880bcc4
Callum Marriage <callum.marriage@capgemini.com>
2018-05-15 16:28:32 +0100

    basic gradle

---
+++ src/main/java/com/team2/Application.java
@@ -0,0 +1,11 @@
+package com.team2;
+
+/**
+ * Created by callummarriage on 15/05/2018.
+ */
+public class Application {
+
+    public void main(String[] args){
+        System.out.println("Hell world");
+    }
+}
