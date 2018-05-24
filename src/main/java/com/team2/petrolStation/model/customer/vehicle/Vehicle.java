db3816146b11155885d91e55ce84a9737859668d
Callum Marriage <callum.marriage@capgemini.com>
2018-05-24 10:30:04 +0100

    Added empty classes

---
+++ src/main/java/com/team2/petrolStation/model/customer/vehicle/Vehicle.java
@@ -0,0 +1,9 @@
+package com.team2.petrolStation.model.customer.vehicle;
+
+import com.team2.petrolStation.model.customer.Customer;
+
+/**
+ * Created by callummarriage on 24/05/2018.
+ */
+public interface Vehicle extends Customer {
+}
---
+++ src/main/java/com/team2/petrolStation/model/serviceMachine/Till.java
@@ -0,0 +1,7 @@
+package com.team2.petrolStation.model.serviceMachine;
+
+/**
+ * Created by callummarriage on 24/05/2018.
+ */
+public class Till extends AbstractServiceMachine {
+}
