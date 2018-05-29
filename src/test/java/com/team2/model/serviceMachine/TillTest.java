4c060a9019e76c261430e1d0f7b7862634de3516
Callum Marriage <callum.marriage@capgemini.com>
2018-05-29 20:47:04 +0100

    created test classes

---
+++ src/test/java/com/team2/model/serviceMachine/TillTest.java
@@ -0,0 +1,11 @@
+package com.team2.model.serviceMachine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by callummarriage on 29/05/2018.
+ */
+public class TillTest {
+}
